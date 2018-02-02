db40aaec9d5e9494f79a2712ae39aca53885197e
miaoximeng <miaoximeng@ucfgroup.com>
2018-02-02 15:14:05 +0800

    update

--- android/src/main/java/com/omron/ble/device/DeviceType.java
+++ android/src/main/java/com/omron/ble/device/DeviceType.java
@@ -3,8 +3,7 @@ package com.omron.ble.device;
 
 public enum DeviceType {
     HGM_124T("BLEsmart_00090006", 1),
-    HGM_124TS("HGM-124T", 1),
-//    HGM_124TSs(HGM_124TS("HGM-124T", 1) , HGM_124T("BLEsmart_00090006", 1)),
+    HGM_124T_A("HGM-124T", 1),
     HGM_125T("BLEsmart_00090007", 1),
     HGM_126T("BLEsmart_00090008", 1),
     BLOOD_XXX("BLEsmart_00090008", 2);
@@ -29,7 +28,7 @@ public enum DeviceType {
     }
 
     public static DeviceType[] getHGM124T(){
-        return (new DeviceType[] { DeviceType.HGM_124T, DeviceType.HGM_124TS });
+        return (new DeviceType[] { DeviceType.HGM_124T, DeviceType.HGM_124T_A });
     }
 
 }
